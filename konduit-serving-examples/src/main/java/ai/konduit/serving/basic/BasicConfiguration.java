4a316fca0c212bca96c6329be323df6917961dea
shams <shamsazeem20@gmail.com>
2019-10-20 17:24:26 +0500

    Konduit server basic examples

---
+++ konduit-serving-examples/src/main/java/ai/konduit/serving/basic/BasicConfiguration.java
@@ -0,0 +1,9 @@
+package ai.konduit.serving.basic;
+
+import ai.konduit.serving.configprovider.KonduitServingMainArgs;
+
+public class BasicConfiguration {
+    public static void main(String[] args) {
+        
+    }
+}
