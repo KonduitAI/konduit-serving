a7a1474ece21dea2448dfd14789db52af44b86c6
shams <shamsazeem20@gmail.com>
2019-10-25 15:21:52 +0500

    BasicConfiguration Example

--- konduit-serving-examples/src/main/java/ai/konduit/serving/basic/BasicConfiguration.java
+++ konduit-serving-examples/src/main/java/ai/konduit/serving/basic/BasicConfiguration.java
@@ -29,29 +29,48 @@ public class BasicConfiguration {
          */
 
         // Let's create a PipelineStep for a simple transform process that performs mathematical functions on arrays
+
+        // First we'll define the input and output schema with two array inputs (x1 and x2)
         Schema inputSchema = new Schema.Builder()
-                .addColumnNDArray("x1", new long[]{10, 10})
-                .addColumnNDArray("x2", new long[]{5, 5})
+                .addColumnNDArray("x1", new long[]{10, 10}) // x1: 10x10 array
+                .addColumnNDArray("x2", new long[]{5, 5}) // x2: 5x5 array
                 .build();
 
+        /*
+         * Since we'll not start a server and do transformation without it, we don't really need to define
+         * the output schema. But still doing so for completeness. Let's call them y1 and y2.
+         */
         Schema outputSchema = new Schema.Builder()
-                .addColumnNDArray("y1", new long[]{10, 10})
-                .addColumnNDArray("y2", new long[]{5, 5})
+                .addColumnNDArray("y1", new long[]{10, 10}) // y1: 10x10 array
+                .addColumnNDArray("y2", new long[]{5, 5}) // y2: 5x5 array
                 .build();
 
+        /*
+         * Then we'll define a transform process that operates on the defined inputs.
+         */
         TransformProcess transformProcess = new TransformProcess.Builder(inputSchema)
-                .ndArrayScalarOpTransform("x1", MathOp.Add, 20.0)
-                .ndArrayScalarOpTransform("x2", MathOp.Divide, 20.0)
-                .ndArrayMathFunctionTransform("x2", MathFunction.SIN)
-                .ndArrayMathFunctionTransform("x2", MathFunction.FLOOR)
-                .ndArrayScalarOpTransform("x2", MathOp.Add, 2)
+                .ndArrayScalarOpTransform("x1", MathOp.Add, 20.0) // Adds 20.0 to array elements
+                .ndArrayScalarOpTransform("x2", MathOp.Divide, 20.0) // Divides each element by 20.0
+                .ndArrayMathFunctionTransform("x2", MathFunction.SIN) // Applies sin() function to each element
+                .ndArrayMathFunctionTransform("x2", MathFunction.FLOOR) // Floors each element
+                .ndArrayScalarOpTransform("x2", MathOp.Add, 2) // Adds 2 to each element
                 .build();
 
+        /*
+         * Now we'll create the pipeline step for the transform process
+         */
         TransformProcessPipelineStep transformProcessPipelineStep = new TransformProcessPipelineStep()
                 .step("default", inputSchema, outputSchema, transformProcess);
 
+        /*
+         * TransformProcessPipelineStepRunner is the executor for the transform process step that we defined above.
+         * We'll put inputs to its execute function and get our desired output.
+         */
         TransformProcessPipelineStepRunner transformProcessPipelineStepRunner = new TransformProcessPipelineStepRunner(transformProcessPipelineStep);
 
+        /*
+         * Now creating a test record input
+         */
         Record[] output = transformProcessPipelineStepRunner.transform(
                 new Record[] {
                         new org.datavec.api.records.impl.Record(
