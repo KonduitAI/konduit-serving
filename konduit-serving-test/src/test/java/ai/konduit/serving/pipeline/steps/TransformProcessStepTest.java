ef0028f86653bd8a3444113bfe94c562f87bb9c0
Max Pumperla <max.pumperla@googlemail.com>
2019-10-28 15:05:28 +0100

    more tests

--- konduit-serving-test/src/test/java/ai/konduit/serving/pipeline/steps/TransformProcessStepTest.java
+++ konduit-serving-test/src/test/java/ai/konduit/serving/pipeline/steps/TransformProcessStepTest.java
@@ -1,12 +1,15 @@
 package ai.konduit.serving.pipeline.steps;
 
+import ai.konduit.serving.config.SchemaType;
 import ai.konduit.serving.pipeline.TransformProcessPipelineStep;
 import ai.konduit.serving.train.TrainUtils;
+import ai.konduit.serving.util.SchemaTypeUtils;
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.schema.Schema;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static ai.konduit.serving.train.TrainUtils.getIrisOutputSchema;
 
@@ -29,6 +32,15 @@ public class TransformProcessStepTest {
 
         assert tpps.getInputNames().contains("foo");
         assert tpps.getInputNames().contains("bar");
+
+        assert tpps.getOutputNames().contains("foo");
+        assert tpps.getOutputNames().contains("bar");
+
+        TransformProcessPipelineStep tpps2 = new TransformProcessPipelineStep(inputSchema, outputSchema, tp);
+
+        assert  tpps2.getTransformProcesses().get("default") == tp;
+
+        assert Arrays.equals(tpps2.getInputSchemas().get("default"), SchemaTypeUtils.typesForSchema(inputSchema));
 
     }
 }
