a7b43f909091fc76a5a0c3952d10b8cf3163474e
Max Pumperla <max.pumperla@googlemail.com>
2019-11-12 20:07:53 +0100

    clean example

--- python/examples/face_detection_pytorch/konduit_client.py
+++ python/examples/face_detection_pytorch/konduit_client.py
@@ -1,55 +1,25 @@
 from konduit.client import Client
-
-import numpy as np
-import time
-import random
-
-import sys
 import base64
-import cv2
-import json
+import os
 
-from jnius import autoclass
-
-input_names = ['default']
-output_names = ['default']
-#port = random.randint(1000, 65535)
-port = 9233
-
-client = Client(input_names=input_names,
-                output_names=output_names,
+client = Client(input_names=['default'],
+                output_names=['default'],
                 input_type='JSON',
                 return_output_type='JSON',
                 endpoint_output_type='RAW',
-                url='http://localhost:' + str(port))
+                url='http://localhost:1337')
 
-def encodeToBase64(filename):
+
+def to_base_64(filename):
     with open(filename, "rb") as f:
         img_base64 = base64.b64encode(f.read())
-    return str(img_base64)[2:-1]  # remove b'...'
+    return str(img_base64)[2:-1]
 
-"""
-workdir = '/home/ubuntu/work/Ultra-Light-Fast-Generic-Face-Detector-1MB'
-image = encodeToBase64(workdir + '/imgs/1.jpg')
-#orig_image = cv2.imread(workdir + '/imgs/1.jpg')
-#image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
-#image = image.astype(np.float32)
-#print(image.shape)
-#image /= 255.
-print('type(image)', type(image))
-data_input = {
-    #'default': np.load('./data/input-0.npy'),
-    'default': image
-}
-#print(type(orig_image))
-"""
-image = 'test'
-#ArrayList = autoclass("java.util.ArrayList")
+workdir = os.path.abspath('./Ultra-Light-Fast-Generic-Face-Detector-1MB')
+encoded_image = to_base_64(os.path.join(workdir, 'imgs/1.jpg'))
+
 try:
-    #predicted = client.predict(data_input)
-    predicted = client.predict({'default': 'test'})
+    predicted = client.predict( {'default': encoded_image})
     print(predicted)
-    #server.stop()
 except Exception as e:
     print(e)
-    #server.stop()
--- python/examples/face_detection_pytorch/konduit_server.py
+++ python/examples/face_detection_pytorch/konduit_server.py
@@ -1,51 +1,27 @@
 from konduit import *
 from konduit.server import Server
-from konduit.client import Client
-from konduit.utils import is_port_in_use
 
-import numpy as np
-import time
-import random
 import os
 import sys
 
 python_path = ':'.join(sys.path)
 workdir = os.path.abspath('./Ultra-Light-Fast-Generic-Face-Detector-1MB')
 python_path += ':' + workdir
-print(python_path)
 
-input_names = ['default']
-output_names = ['default']
-port = random.randint(1000, 65535)
-port = 9233
-parallel_inference_config = ParallelInferenceConfig(workers=1)
-serving_config = ServingConfig(http_port=port,
-                               input_data_type='JSON',
-                               output_data_type='JSON',
-                               log_timings=True)
+port = 1337
+serving_config = ServingConfig(http_port=port, input_data_type='JSON', output_data_type='JSON')
 
 python_config = PythonConfig(
-    python_path=python_path,
-    python_code_path=workdir+'/detect_one.py',
-    python_inputs={'default': 'STR'},
-    python_outputs={'default': 'STR'},
+    python_path=python_path, python_code_path=os.path.join(workdir, 'detect_one.py'),
+    python_inputs={'default': 'STR'}, python_outputs={'default': 'STR'},
 )
 
-python_pipeline_step = PythonPipelineStep(input_names=input_names,
-                                          output_names=output_names,
-                                          input_schemas=({'default': ['String']}),
-                                          output_schemas=({'default': ['String']}),
-                                          input_column_names={'default': ['default']},
-                                          output_column_names={'default': ['default']},
-                                          python_configs={'default': python_config})
+python_pipeline_step = PythonPipelineStep().step(
+    python_config=python_config, input_types=['String'], input_column_names=['default'],
+    output_types=['String'], output_column_names=['default'], step_name="default"
+)
 
-inference = InferenceConfiguration(serving_config=serving_config,
-                                   pipeline_steps=[python_pipeline_step])
+inference = InferenceConfiguration(serving_config=serving_config, pipeline_steps=[python_pipeline_step])
 
-server = Server(config=inference,
-                #extra_start_args='-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005',  # intellij remote debug
-                extra_start_args='-Xmx8g',
-                jar_path='konduit.jar')
+server = Server(config=inference, extra_start_args='-Xmx8g')
 server.start()
-print('Process started.')
-time.sleep(10)
