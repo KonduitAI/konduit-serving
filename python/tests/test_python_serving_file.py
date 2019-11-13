8a37852123df81255b8d9cede72a27b4f9547fcd
Max Pumperla <max.pumperla@googlemail.com>
2019-11-13 19:20:00 +0100

    python file serving test

---
+++ python/tests/test_python_serving_file.py
@@ -0,0 +1,33 @@
+from konduit import *
+from konduit.server import Server
+from konduit.client import Client
+from konduit.utils import is_port_in_use, default_python_path
+
+import numpy as np
+import os
+
+
+def test_python_script_prediction():
+    port = 1337
+    serving_config = ServingConfig(http_port=port)
+    work_dir = os.path.abspath('.')
+
+    python_config = PythonConfig(
+        python_path=default_python_path(work_dir), python_code_path=os.path.join(work_dir, 'simple.py'),
+        python_inputs={'first': 'NDARRAY'}, python_outputs={'second': 'NDARRAY'}
+    )
+
+    step = PythonPipelineStep().step(python_config)
+    server = Server(steps=step, serving_config=serving_config)
+    server.start(sleep=10)
+    
+    client = Client(url='http://localhost:' + str(port))
+
+    if is_port_in_use(port):
+        input_array = np.load('../data/input-0.npy')
+        predicted = client.predict(input_array)
+        print(predicted)
+        server.stop()
+    else:
+        server.stop()
+        raise Exception("Server not running on specified port")
