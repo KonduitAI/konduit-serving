b633bcaaa28da4528ceb1cc943ff5ee319af4d43
Max Pumperla <max.pumperla@googlemail.com>
2019-11-25 16:35:47 +0100

    refactor test, documentation, standalone java example

--- python/tests/prepare_doc_tests.sh
+++ python/tests/prepare_doc_tests.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# copy konduit jar from base folder (assuming it has been generated there
+cp ../../konduit.jar .
+
+# run the "prepare java files" part from the unit test
 python test_docs.py
+
+# compile the java source files
 javac -cp ./konduit.jar -d . ai/konduit/serving/*.java
-jar uf konduit.jar ai/konduit/serving/BasicsTest*.class
+
+# only keep the compiled files
+rm ai/konduit/serving/*.java
+
+# update the jar with all new test classes
+jar uf konduit.jar ai/konduit/serving/*
