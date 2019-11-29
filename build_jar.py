5eb89b2e055cc55d2f94e797c4737d91b4fa47ff
Max Pumperla <max.pumperla@googlemail.com>
2019-11-29 13:02:56 +0100

    fixes #106

--- build_jar.py
+++ build_jar.py
@@ -3,60 +3,89 @@ import subprocess
 from shutil import copyfile
 import os
 import re
-from distutils.util import strtobool
+import distutils
 
 
-if __name__ == '__main__':
-    '''
+if __name__ == "__main__":
+    """
     Example:
 
     ./build_jar.py --os linux-x86_64
-    '''
-    parser = argparse.ArgumentParser(description='Build a Konduit JAR.')
+    """
+    parser = argparse.ArgumentParser(description="Build a Konduit JAR.")
 
-    parser.add_argument('--os', type=str,
-                        required = True, 
-                        choices = ['windows-x86_64', 'linux-x86_64', 'linux-x86_64-gpu', 
-                                   'macosx-x86_64', 'linux-armhf', 'windows-x86_64-gpu'],
-                        help='the javacpp.platform to use: windows-x86_64,linux-x86_64,linux-x86_64-gpu'
-                            ' macosx-x86_64,linux-armhf,windows-x86_64-gpu ')
+    parser.add_argument(
+        "--os",
+        type=str,
+        required=True,
+        choices=[
+            "windows-x86_64",
+            "linux-x86_64",
+            "linux-x86_64-gpu",
+            "macosx-x86_64",
+            "linux-armhf",
+            "windows-x86_64-gpu",
+        ],
+        help="the javacpp.platform to use: windows-x86_64,linux-x86_64,linux-x86_64-gpu"
+        " macosx-x86_64,linux-armhf,windows-x86_64-gpu ",
+    )
 
-    parser.add_argument('--usePython', type=str, default='true',
-                        help='whether to bundle python '
-                            'or not (typically not encouraged with arm')
+    parser.add_argument(
+        "--usePython",
+        type=str,
+        default="true",
+        help="whether to bundle python " "or not (typically not encouraged with arm",
+    )
 
-    parser.add_argument('--usePmml', type=str, default='true',
-                        help='whether to use pmml or not,'
-                            ' not encouraged if agpl license is an issue')
+    parser.add_argument(
+        "--usePmml",
+        type=str,
+        default="true",
+        help="whether to use pmml or not,"
+        " not encouraged if agpl license is an issue",
+    )
 
-    parser.add_argument('--source', type=str,
-                        help='the path to the model server',default='.')
+    parser.add_argument(
+        "--source", type=str, help="the path to the model server", default="."
+    )
 
-    parser.add_argument('--target', type=str,
-                        help='the path to the model server output',default='konduit.jar')
+    parser.add_argument(
+        "--target",
+        type=str,
+        help="the path to the model server output",
+        default="konduit.jar",
+    )
 
     args = parser.parse_args()
-    command = args.source + os.sep + 'mvnw -Puberjar clean install -Dmaven.test.skip=true'
-    command += ' -Djavacpp.platform=' + args.os + ' '
-    if 'arm' in args.os:
-        command += '-Dchip=arm'
-    elif 'gpu' in args.os:
-        command += ' -Dchip=gpu'
+    command = (
+        args.source + os.sep + "mvnw -Puberjar clean install -Dmaven.test.skip=true"
+    )
+    command += " -Djavacpp.platform=" + args.os + " "
+    if "arm" in args.os:
+        command += "-Dchip=arm"
+    elif "gpu" in args.os:
+        command += " -Dchip=gpu"
     else:
-        command += ' -Dchip=cpu'
-    if strtobool(args.usePython):
-        command += ' -Ppython'
-    if strtobool(args.usePmml):
-        command += ' -Ppmml'
+        command += " -Dchip=cpu"
+    if distutils.util.strtobool(args.usePython):
+        command += " -Ppython"
+    if distutils.util.strtobool(args.usePmml):
+        command += " -Ppmml"
 
-    with open(os.path.join(args.source, 'pom.xml'), 'r') as pom:
+    with open(os.path.join(args.source, "pom.xml"), "r") as pom:
         content = pom.read()
         regex = r"<version>(\d+.\d+.\d+)</version>"
         version = re.findall(regex, content)
 
-    print('Running command: ' + command)
+    print("Running command: " + command)
     subprocess.run(command, cwd=args.source, shell=True, check=True)
-    copyfile(os.path.join(args.source, 'konduit-serving-uberjar', 'target',
-                          'konduit-serving-uberjar-{}-bin.jar'.format(version[0])), args.target
+    copyfile(
+        os.path.join(
+            args.source,
+            "konduit-serving-uberjar",
+            "target",
+            "konduit-serving-uberjar-{}-bin.jar".format(version[0]),
+        ),
+        args.target,
     )
-    copyfile(args.target, os.path.join('python', 'tests', args.target))
+    copyfile(args.target, os.path.join(args.source, "python", "tests", args.target))
