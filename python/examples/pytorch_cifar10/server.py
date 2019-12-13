fd6c7cd57b78679b5cfab3014910af4d51778f79
Max Pumperla <max.pumperla@googlemail.com>
2019-12-13 09:59:27 +0100

    image loading example port

---
+++ python/examples/pytorch_cifar10/server.py
@@ -0,0 +1,35 @@
+import torch
+from model.train import Model
+import torchvision.transforms as transforms
+
+transform = None
+model = None
+second = None
+
+
+def setup():
+    global transform
+    global model
+
+    transform = transforms.Compose(
+        [  # transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]
+    )
+    model = Model()
+    model.load_state_dict(torch.load("./model/model.pt"))
+    model.eval()
+
+
+def run():
+    global transform
+    global model
+    global first
+    global second
+
+    input_data = transform(torch.from_numpy(first).squeeze(0)).unsqueeze(0)
+    second = model(input_data).detach().numpy()
+
+
+setup()
+run()
