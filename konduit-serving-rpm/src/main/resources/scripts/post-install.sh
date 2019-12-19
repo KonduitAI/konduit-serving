2a62c96c8374205b5fd5eb0fd68a012ffbe87dd8
shams <shamsazeem20@gmail.com>
2019-12-20 00:14:33 +0500

    Sending both stdout and stderr to parent process

--- konduit-serving-rpm/src/main/resources/scripts/post-install.sh
+++ konduit-serving-rpm/src/main/resources/scripts/post-install.sh
@@ -4,6 +4,6 @@
 
 chmod u+x "${KONDUIT_SERVING_BIN_DIRECTORY}"/konduit-serving
 # Installing miniconda and other package
-sh "${KONDUIT_SERVING_HOME}"/install-python.sh >/dev/tty
+sh "${KONDUIT_SERVING_HOME}"/install-python.sh >/dev/tty 2>&1 # Sending stdout to the parent process and sending stderr to stdout...
 
 exit 0
