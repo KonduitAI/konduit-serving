b1b9d30706e3337173a4386180dc1faf9f9834c3
shams <shamsazeem20@gmail.com>
2019-12-19 22:14:12 +0500

    Fixed environment variables and pre-remove script

--- konduit-serving-rpm/src/main/resources/scripts/pre-remove.sh
+++ konduit-serving-rpm/src/main/resources/scripts/pre-remove.sh
@@ -5,7 +5,8 @@ if [ -z "${KONDUIT_SERVING_LOG_DIR}" ]; then
 fi
 
 if [ -z "${KONDUIT_SERVING_HOME}" ]; then
-  rm -rf "${KONDUIT_SERVING_HOME}"
+  rm  "${KONDUIT_SERVING_HOME}"/konduit-serving-*.jar
+  rm  "${KONDUIT_SERVING_HOME}"/install-python.sh
 fi
 
 rm /etc/profile.d/konduit-serving-env.sh
