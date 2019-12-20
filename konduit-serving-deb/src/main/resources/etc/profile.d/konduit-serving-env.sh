7e7dbdb1116cc8aa2f5ef49900482abaf839a3be
shams <shamsazeem20@gmail.com>
2019-12-20 20:06:09 +0500

    Deb package initial setup

---
+++ konduit-serving-deb/src/main/resources/etc/profile.d/konduit-serving-env.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# This file defines the environment variables needed to run the konduit serving admin command.
+
+export KONDUIT_SERVING_BASE_ARTIFACT_NAME=konduit-serving
+export KONDUIT_SERVING_DIRECTORY_STRUCTURE=konduit/serving
+export KONDUIT_SERVING_SPIN=minimal
+export KONDUIT_SERVING_VERSION=0.1.0-SNAPSHOT
+export KONDUIT_SERVING_BACKEND=cpu
+
+export KONDUIT_SERVING_HOME=/opt/${KONDUIT_SERVING_DIRECTORY_STRUCTURE}
+export CONDA_INSTALL_PATH=${KONDUIT_SERVING_HOME}/miniconda
+export KONDUIT_SERVING_LOG_DIR=/var/log/${KONDUIT_SERVING_DIRECTORY_STRUCTURE}
+export KONDUIT_JAR_PATH=${KONDUIT_SERVING_HOME}/${KONDUIT_SERVING_BASE_ARTIFACT_NAME}-${KONDUIT_SERVING_VERSION}.jar
+export KONDUIT_SERVING_PID_FILE=${KONDUIT_SERVING_HOME}/${KONDUIT_SERVING_BASE_ARTIFACT_NAME}.pid
+export KONDUIT_SERVING_BIN_DIRECTORY=${KONDUIT_SERVING_HOME}/bin
+
+if [ -z "$JAVA_HOME" ]; then
+  export JAVA_HOME=/usr/lib/jvm/java
+fi
+
+if [ -z "$JDK_HOME" ]; then
+  export JDK_HOME=/usr/lib/jvm/java
+fi
+
+export PATH=${KONDUIT_SERVING_BIN_DIRECTORY}:${PATH}
--- konduit-serving-rpm/src/main/resources/scripts/post-install.sh
+++ konduit-serving-rpm/src/main/resources/scripts/post-install.sh
@@ -6,4 +6,6 @@ chmod u+x "${KONDUIT_SERVING_BIN_DIRECTORY}"/konduit-serving
 # Installing miniconda and other package
 sh "${KONDUIT_SERVING_HOME}"/install-python.sh >/dev/tty 2>&1 # Sending stdout to the parent process and sending stderr to stdout...
 
+chown -R konduit:konduit /opt/konduit
+
 exit 0
