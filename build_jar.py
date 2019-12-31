7ac4cda58e4718641ef32b613c1681f2461ec0f7
Philip Khor <35039795+philip-khor@users.noreply.github.com>
2019-12-31 09:44:50 +0800

    Redundant "gpu" in filename copied from target
    The naming scheme for uberjars truncates the chip from the os argument only for GPU `javacpp.platform`s. This results in the following error on GPU builds: 
    
    ```
    Traceback (most recent call last):                                                                                        File "C:\Users\Skymind AI Berhad\.konduit\konduit-serving\build_jar.py", line 105, in <module>                            os.path.join(args.source, args.target),                                                                               File "c:\users\skymind ai berhad\appdata\local\continuum\miniconda3\lib\shutil.py", line 120, in copyfile                 with open(src, 'rb') as fsrc:                                                                                       FileNotFoundError: [Errno 2] No such file or directory: 'C:\\Users\\Skymind AI Berhad\\.konduit\\konduit-serving\\konduit-serving-uberjar\\target\\konduit-serving-uberjar-0.1.0-SNAPSHOT-all-windows-x86_64-gpu-gpu.jar'       
    ```
    
    This fix tested by running python build_jar.py with os arguments windows-x86_64, windows-x86_64-gpu, linux-armhf

--- build_jar.py
+++ build_jar.py
@@ -93,6 +93,8 @@ if __name__ == "__main__":
     print("Running command: " + " ".join(command))
     subprocess.call(command, shell=sys.platform.startswith('win'), cwd=args.source)
 
+    _os = args.os[:-4] if arch == "gpu" else args.os
+
     # Copy the jar file to the path specified by the "target" argument
     copyfile(
         os.path.join(
@@ -100,7 +102,7 @@ if __name__ == "__main__":
             "konduit-serving-uberjar",
             "target",
             "konduit-serving-uberjar-{}-{}-{}-{}.jar"\
-            .format(version[0], args.spin, args.os, arch),
+            .format(version[0], args.spin, _os, arch),
         ),
         os.path.join(args.source, args.target),
     )
