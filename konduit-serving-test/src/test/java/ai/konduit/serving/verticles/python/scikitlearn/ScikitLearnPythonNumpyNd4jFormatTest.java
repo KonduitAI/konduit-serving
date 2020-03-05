df444a271a40af81f5938d1a46b9acd8e2809add
venkat-nidrive <venkateswarlu@nidrive.jp>
2020-03-05 19:36:26 +0900

    Numpy as output format and ND4J as input format for all 4 frameworks .Yet to test completely.

--- konduit-serving-test/src/test/java/ai/konduit/serving/verticles/python/scikitlearn/ScikitLearnPythonNumpyNd4jFormatTest.java
+++ konduit-serving-test/src/test/java/ai/konduit/serving/verticles/python/scikitlearn/ScikitLearnPythonNumpyNd4jFormatTest.java
@@ -17,17 +17,18 @@
  *  *  * SPDX-License-Identifier: Apache-2.0
  *  *  *****************************************************************************
  *
- * */
+ *
+ */
 
-package ai.konduit.serving.verticles.python.tensorFlow;
+package ai.konduit.serving.verticles.python.scikitlearn;
 
 import ai.konduit.serving.InferenceConfiguration;
 import ai.konduit.serving.config.Output;
 import ai.konduit.serving.config.ServingConfig;
 import ai.konduit.serving.miscutils.PythonPathInfo;
 import ai.konduit.serving.model.PythonConfig;
-import ai.konduit.serving.pipeline.step.ImageLoadingStep;
 import ai.konduit.serving.pipeline.step.PythonStep;
+import ai.konduit.serving.util.ExpectedAssertTest;
 import ai.konduit.serving.verticles.inference.InferenceVerticle;
 import ai.konduit.serving.verticles.numpy.tensorflow.BaseMultiNumpyVerticalTest;
 import com.jayway.restassured.specification.RequestSpecification;
@@ -38,27 +39,30 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.apache.commons.io.FileUtils;
-import org.datavec.api.writable.NDArrayWritable;
-import org.datavec.api.writable.Writable;
-import org.datavec.image.transform.ImageTransformProcess;
 import org.datavec.python.PythonType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.io.ClassPathResource;
+import org.nd4j.serde.binary.BinarySerde;
 
 import javax.annotation.concurrent.NotThreadSafe;
 import java.io.File;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.bytedeco.cpython.presets.python.cachePackages;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(VertxUnitRunner.class)
 @NotThreadSafe
-public class TensorFlowPythonNumpyNumpyFormatTest extends BaseMultiNumpyVerticalTest {
+public class ScikitLearnPythonNumpyNd4jFormatTest extends BaseMultiNumpyVerticalTest {
 
     @Override
     public Class<? extends AbstractVerticle> getVerticalClazz() {
@@ -79,19 +83,23 @@ public class TensorFlowPythonNumpyNumpyFormatTest extends BaseMultiNumpyVertical
 
     @Override
     public JsonObject getConfigObject() throws Exception {
-        String pythonCodePath = new ClassPathResource("scripts/tensorflow/TensorFlowImageTest.py").getFile().getAbsolutePath();
+        String pythonPath = Arrays.stream(cachePackages())
+                .filter(Objects::nonNull)
+                .map(File::getAbsolutePath)
+                .collect(Collectors.joining(File.pathSeparator));
+        String pythonCodePath = new ClassPathResource("scripts/scikitlearn/NumpyScikitLearnNumpy.py").getFile().getAbsolutePath();
 
         PythonConfig pythonConfig = PythonConfig.builder()
-                .pythonCodePath(pythonCodePath)
                 .pythonPath(PythonPathInfo.getPythonPath())
-                .pythonInput("img", PythonType.TypeName.NDARRAY.name())
-                .pythonOutput("prediction", PythonType.TypeName.NDARRAY.name())
+                .pythonCodePath(pythonCodePath)
+                .pythonInput("inputValue", PythonType.TypeName.NDARRAY.name())
+                .pythonOutput("outputValue", PythonType.TypeName.NDARRAY.name())
                 .build();
 
         PythonStep pythonStepConfig = new PythonStep(pythonConfig);
 
         ServingConfig servingConfig = ServingConfig.builder()
-                .outputDataFormat(Output.DataFormat.NUMPY)
+                .outputDataFormat(Output.DataFormat.ND4J)
                 .httpPort(port)
                 .build();
 
@@ -103,94 +111,71 @@ public class TensorFlowPythonNumpyNumpyFormatTest extends BaseMultiNumpyVertical
         return new JsonObject(inferenceConfiguration.toJson());
     }
 
-    @Test
+    @Test(timeout = 60000)
     public void testInferenceResult(TestContext context) throws Exception {
         this.context = context;
         RequestSpecification requestSpecification = given();
         requestSpecification.port(port);
-        JsonObject jsonObject = new JsonObject();
 
-        ImageTransformProcess imageTransformProcess = new ImageTransformProcess.Builder()
-                .scaleImageTransform(20.0f)
-                .resizeImageTransform(28, 28)
-                .build();
+        //Preparing input NDArray
+        INDArray arr = Nd4j.create(new float[][]{{1, 0, 5, 10}, {100, 55, 555, 1000}});
 
-        ImageLoadingStep imageLoadingStep = ImageLoadingStep.builder()
-                .imageProcessingInitialLayout("NCHW")
-                .imageProcessingRequiredLayout("NHWC")
-                .inputName("default")
-                .dimensionsConfig("default", new Long[]{28L, 28L, 1L}) // Height, width, channels
-                .imageTransformProcess("default", imageTransformProcess)
-                .build();
+        byte[] xNpy = Nd4j.toNpyByteArray(arr);
 
-        String imagePath = new ClassPathResource("data/5.png").getFile().getAbsolutePath();
-
-        Writable[][] output = imageLoadingStep.createRunner().transform(imagePath);
-
-        INDArray image = ((NDArrayWritable) output[0][0]).get();
-
-        byte[] sciNpy = Nd4j.toNpyByteArray(image);
-
-        File tensFile = temporary.newFile();
-        FileUtils.writeByteArrayToFile(tensFile, sciNpy);
+        File xFile = temporary.newFile();
+        FileUtils.writeByteArrayToFile(xFile, xNpy);
 
         requestSpecification.header("Content-Type", "multipart/form-data");
         String response = requestSpecification.when()
-                .multiPart("default", tensFile)
+                .multiPart("default", xFile)
                 .expect().statusCode(200)
                 .body(not(isEmptyOrNullString()))
                 .post("/raw/numpy").then()
                 .extract()
                 .body().asString();
-        //TODO:assertion yet to implement.
-        System.out.print(response);
-        /*INDArray outputArray=  convertToNd4J(response);
-        assertEquals(7, outputArray.getDouble(0), 1e-1);*/
+
+        File outputImagePath = new File(
+                "src/main/resources/data/test-nd4j-output.zip");
+        FileUtils.writeStringToFile(outputImagePath, response, Charset.defaultCharset());
+        System.out.println(BinarySerde.readFromDisk(outputImagePath));
+        INDArray outputArray = BinarySerde.readFromDisk(outputImagePath);
+        INDArray expectedArr = ExpectedAssertTest.NdArrayAssert("src/test/resources/Json/scikitlearn/ScikitlearnNumpyTest.json", "raw");
+        assertEquals(expectedArr.getInt(0), outputArray.getInt(0));
+        assertEquals(expectedArr, outputArray);
     }
 
-   // @Test
+   // @Test(timeout = 60000)
     public void testInferenceClassificationResult(TestContext context) throws Exception {
         this.context = context;
         RequestSpecification requestSpecification = given();
         requestSpecification.port(port);
         JsonObject jsonObject = new JsonObject();
 
-        ImageTransformProcess imageTransformProcess = new ImageTransformProcess.Builder()
-                .scaleImageTransform(20.0f)
-                .resizeImageTransform(28, 28)
-                .build();
+        //Preparing input NDArray
+        INDArray arr = Nd4j.create(new float[][]{{1, 0, 5, 10}, {100, 55, 555, 1000}});
 
-        ImageLoadingStep imageLoadingStep = ImageLoadingStep.builder()
-                .imageProcessingInitialLayout("NCHW")
-                .imageProcessingRequiredLayout("NHWC")
-                .inputName("default")
-                .dimensionsConfig("default", new Long[]{28L, 28L, 1L}) // Height, width, channels
-                .imageTransformProcess("default", imageTransformProcess)
-                .build();
+        byte[] xNpy = Nd4j.toNpyByteArray(arr);
 
-        String imagePath = new ClassPathResource("data/5.png").getFile().getAbsolutePath();
-        Writable[][] output = imageLoadingStep.createRunner().transform(imagePath);
-        INDArray image = ((NDArrayWritable) output[0][0]).get();
-        image.castTo(DataType.INT8);
-
-        byte[] sciNpy = Nd4j.toNpyByteArray(image);
-
-        File tensFile = temporary.newFile();
-        FileUtils.writeByteArrayToFile(tensFile, sciNpy);
+        File xFile = temporary.newFile();
+        FileUtils.writeByteArrayToFile(xFile, xNpy);
 
         requestSpecification.header("Content-Type", "multipart/form-data");
         String response = requestSpecification.when()
-                .multiPart("default", tensFile)
+                .multiPart("default", xFile)
                 .expect().statusCode(200)
                 .body(not(isEmptyOrNullString()))
                 .post("/classification/numpy").then()
                 .extract()
                 .body().asString();
-        //TODO:assertion yet to implement.
-        System.out.print(response);
-    /*    INDArray outputArray=  convertToNd4J(response);
-        double outpuValue = outputArray.getDouble(0);
-        assertEquals(7, outpuValue, 1e-1);*/
+
+        File outputImagePath = new File(
+                "src/main/resources/data/test-nd4j-output.zip");
+        FileUtils.writeStringToFile(outputImagePath, response, Charset.defaultCharset());
+        System.out.println(BinarySerde.readFromDisk(outputImagePath));
+        INDArray outputArray = BinarySerde.readFromDisk(outputImagePath);
+        INDArray expectedArr = ExpectedAssertTest.NdArrayAssert("src/test/resources/Json/scikitlearn/ScikitlearnNumpyTest.json", "classification");
+        assertEquals(expectedArr.getInt(0), outputArray.getInt(0));
+        assertEquals(expectedArr, outputArray);
     }
 
 }
