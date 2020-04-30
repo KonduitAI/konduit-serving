6091e6420375eb04b4d6b5cef0165e6fac7f577c
Alexander Stoyakin <alexander.stoyakin@gmail.com>
2020-04-30 17:05:40 +0300

    Generate protobuf schema

--- konduit-serving-api/src/test/java/ai/konduit/serving/data/GeneratedClassesTest.java
+++ konduit-serving-api/src/test/java/ai/konduit/serving/data/GeneratedClassesTest.java
@@ -2,10 +2,11 @@ package ai.konduit.serving.data;
 
 import org.junit.Test;
 
+import generated.Data;
 import static org.junit.Assert.assertEquals;
 
 public class GeneratedClassesTest {
-    /*@Test
+    @Test
     public void testsDataSchemeBuilder() {
         Data.DataScheme innerScheme = Data.DataScheme.newBuilder().
                 setKey("inner").
@@ -19,6 +20,28 @@ public class GeneratedClassesTest {
         assertEquals("inner", innerScheme.getKey());
         assertEquals("inner", scheme.getMetaData().getKey());
         assertEquals("keygen", scheme.getKey());
-    }*/
+    }
 
+    @Test
+    public void testStringData() {
+        Data.DataScheme container = Data.DataScheme.newBuilder().
+                setKey("ok").
+                setSValue("svalue").
+                setType(Data.DataScheme.ValueType.STRING).build();
+        assertEquals("ok", container.getKey());
+    }
+
+    @Test
+    public void testBoolData() {
+        Data.DataScheme container = Data.DataScheme.newBuilder().
+                setKey("ok").
+                setBoolValue(false).
+                setSValue("svalue").
+                setType(Data.DataScheme.ValueType.STRING).build();
+        assertEquals("ok", container.getKey());
+
+        assertEquals("svalue", container.getSValue());
+        assertEquals(false, container.getBoolValue());
+
+    }
 }
