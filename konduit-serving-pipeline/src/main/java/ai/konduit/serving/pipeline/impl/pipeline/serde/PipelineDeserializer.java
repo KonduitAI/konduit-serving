d2718caaa76036c648d4fe407d3b43ec5d307167
Alex Black <blacka101@gmail.com>
2020-05-02 22:45:59 +1000

    Copyright headers
    Signed-off-by: Alex Black <blacka101@gmail.com>

--- konduit-serving-pipeline/src/main/java/ai/konduit/serving/pipeline/impl/pipeline/serde/PipelineDeserializer.java
+++ konduit-serving-pipeline/src/main/java/ai/konduit/serving/pipeline/impl/pipeline/serde/PipelineDeserializer.java
@@ -1,21 +1,36 @@
+/* ******************************************************************************
+ * Copyright (c) 2020 Konduit K.K.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
 package ai.konduit.serving.pipeline.impl.pipeline.serde;
 
 import ai.konduit.serving.pipeline.api.pipeline.Pipeline;
 import ai.konduit.serving.pipeline.api.step.PipelineStep;
 import ai.konduit.serving.pipeline.impl.pipeline.GraphPipeline;
 import ai.konduit.serving.pipeline.impl.pipeline.SequencePipeline;
-import ai.konduit.serving.pipeline.util.ObjectMappers;
 import org.nd4j.shade.jackson.core.JsonParseException;
 import org.nd4j.shade.jackson.core.JsonParser;
 import org.nd4j.shade.jackson.core.JsonProcessingException;
 import org.nd4j.shade.jackson.core.TreeNode;
-import org.nd4j.shade.jackson.core.type.TypeReference;
 import org.nd4j.shade.jackson.databind.DeserializationContext;
-import org.nd4j.shade.jackson.databind.JsonNode;
 import org.nd4j.shade.jackson.databind.deser.std.StdDeserializer;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class PipelineDeserializer extends StdDeserializer<Pipeline> {
 
