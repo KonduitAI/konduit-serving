fa557fa1666b1d0d8673aa71d91dcc3b85dfc638
shamsulazeem <shamsazeem20@gmail.com>
2020-05-19 08:10:53 +0500

    Adding mqtt module structure and relevant todos

---
+++ konduit-serving-vertx-protocols/konduit-serving-mqtt/src/test/java/ai/konduit/serving/vertx/protocols/mqtt/InferenceVerticleMqttTest.java
@@ -0,0 +1,76 @@
+/*
+ *  ******************************************************************************
+ *  * Copyright (c) 2020 Konduit K.K.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Apache License, Version 2.0 which is available at
+ *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  * License for the specific language governing permissions and limitations
+ *  * under the License.
+ *  *
+ *  * SPDX-License-Identifier: Apache-2.0
+ *  *****************************************************************************
+ */
+
+package ai.konduit.serving.vertx.protocols.mqtt;
+
+import ai.konduit.serving.pipeline.impl.pipeline.SequencePipeline;
+import ai.konduit.serving.pipeline.impl.step.logging.LoggingPipelineStep;
+import ai.konduit.serving.vertx.api.DeployKonduitServing;
+import ai.konduit.serving.vertx.config.InferenceConfiguration;
+import ai.konduit.serving.vertx.config.InferenceDeploymentResult;
+import ai.konduit.serving.vertx.config.ServerProtocol;
+import com.google.protobuf.InvalidProtocolBufferException;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.event.Level;
+
+public class InferenceVerticleMqttTest {
+    static InferenceConfiguration configuration;
+    static Vertx vertx;
+    static InferenceDeploymentResult inferenceDeploymentResult;
+
+    @BeforeClass
+    public static void setUp(TestContext testContext) {
+        configuration = InferenceConfiguration.builder()
+                .protocol(ServerProtocol.MQTT)
+                .pipeline(SequencePipeline.builder()
+                        .add(LoggingPipelineStep.builder().log(LoggingPipelineStep.Log.KEYS_AND_VALUES).logLevel(Level.ERROR).build())
+                        .build())
+                .build();
+
+        Async async = testContext.async();
+
+        vertx = DeployKonduitServing.deploy(new VertxOptions(),
+                new DeploymentOptions(),
+                configuration,
+                handler -> {
+                    if(handler.succeeded()) {
+                        inferenceDeploymentResult = handler.result();
+                        async.complete();
+                    } else {
+                        testContext.fail(handler.cause());
+                    }
+                });
+    }
+
+    @Test
+    public void testMqttServer(TestContext testContext) throws InvalidProtocolBufferException {
+        // todo: write a basic test for mqtt
+    }
+
+    @AfterClass
+    public static void tearDown(TestContext testContext) {
+        vertx.close(testContext.asyncAssertSuccess());
+    }
+}
