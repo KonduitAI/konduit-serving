20ea26c9bddc05a0d59ea2779e7516c498352d93
Paul Dubs <paul.dubs@gmail.com>
2020-05-29 19:27:50 +0200

    Add json subtype mapping
    Signed-off-by: Paul Dubs <paul.dubs@gmail.com>

--- konduit-serving-io/konduit-serving-camera/src/main/java/ai/konduit/serving/camera/serde/CameraJsonSubTypesMapping.java
+++ konduit-serving-io/konduit-serving-camera/src/main/java/ai/konduit/serving/camera/serde/CameraJsonSubTypesMapping.java
@@ -1,6 +1,7 @@
 package ai.konduit.serving.camera.serde;
 
 import ai.konduit.serving.camera.step.capture.CameraFrameCaptureStep;
+import ai.konduit.serving.camera.step.capture.VideoFrameCaptureStep;
 import ai.konduit.serving.pipeline.api.serde.JsonSubType;
 import ai.konduit.serving.pipeline.api.serde.JsonSubTypesMapping;
 import ai.konduit.serving.pipeline.api.step.PipelineStep;
@@ -13,6 +14,7 @@ public class CameraJsonSubTypesMapping implements JsonSubTypesMapping {
     public List<JsonSubType> getSubTypesMapping() {
         List<JsonSubType> l = new ArrayList<>();
         l.add(new JsonSubType("FRAME_CAPTURE", CameraFrameCaptureStep.class, PipelineStep.class));
+        l.add(new JsonSubType("VIDEO_CAPTURE", VideoFrameCaptureStep.class, PipelineStep.class));
         return l;
     }
 }
