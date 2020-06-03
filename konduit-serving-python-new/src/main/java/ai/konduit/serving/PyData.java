a140cd4a86c97b53776deeff9d974dffd2051c5f
Fariz Rahman <farizrahman4u@gmail.com>
2020-06-03 21:09:22 +0530

    toJava

--- konduit-serving-python-new/src/main/java/ai/konduit/serving/PyData.java
+++ konduit-serving-python-new/src/main/java/ai/konduit/serving/PyData.java
@@ -1,9 +1,14 @@
 package ai.konduit.serving;
 
 import ai.konduit.serving.pipeline.api.data.Data;
-import org.eclipse.python4j.PythonException;
-import org.eclipse.python4j.PythonObject;
-import org.eclipse.python4j.PythonType;
+import ai.konduit.serving.pipeline.api.data.NDArray;
+import ai.konduit.serving.pipeline.impl.data.JData;
+import org.bytedeco.javacpp.BytePointer;
+import org.eclipse.python4j.*;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PyData extends PythonType<Data> {
 
@@ -13,7 +18,107 @@ public class PyData extends PythonType<Data> {
     }
     @Override
     public Data toJava(PythonObject pythonObject) throws PythonException{
-        return null;
+        JData data = new JData();
+        try (PythonGC gc = PythonGC.watch()){
+            PythonObject pyKeysList = Python.list(pythonObject.attr("keys").call());
+            List keysList = PythonTypes.LIST.toJava(pyKeysList);
+            for (Object key: keysList){
+                if (!(key instanceof String)){
+                    throw new PythonException("String key expected."); // already checked in python
+                }
+                String strKey = (String)key;
+                PythonObject val = pythonObject.get(strKey);
+                if (Python.isinstance(val, Python.strType())){
+                    String jVal = PythonTypes.STR.toJava(val);
+                    data.put(strKey, jVal);
+                }
+                else if (Python.isinstance(val, Python.intType())){
+                    long jVal = PythonTypes.INT.toJava(val);
+                    data.put(strKey, jVal);
+                }
+                else if (Python.isinstance(val, Python.floatType())){
+                    double jVal = PythonTypes.FLOAT.toJava(val);
+                    data.put(strKey, jVal);
+                }
+                else if (Python.isinstance(val, Python.boolType())){
+                    boolean jVal = PythonTypes.BOOL.toJava(val);
+                    data.put(strKey, jVal);
+                }
+                else if (Python.isinstance(val, Python.memoryviewType())){
+                    BytePointer bp = PythonTypes.MEMORYVIEW.toJava(val);
+                    byte[] jVal = bp.getStringBytes();
+                    data.put(strKey, jVal);
+                }
+                else if (Python.isinstance(val, Python.importModule("numpy").attr("ndarray"))){
+                    INDArray arr = NumpyArray.INSTANCE.toJava(val);
+                    NDArray jVal = NDArray.create(arr);
+                    data.put(strKey, jVal);
+                }
+                else if (Python.isinstance(val, Python.importModule("PIL.Image").attr("Image"))){
+                    // TODO
+                    throw new PythonException("Image not supprted yet.");
+                }
+                else if (Python.isinstance(val, Python.listType())){
+                    long size = Python.len(val).toLong();
+                    if (size == 0){
+                        throw new PythonException("Cannot infer type from empty list.");
+                    }
+                    PythonObject item0 = val.get(0);
+                    if (Python.isinstance(item0, Python.strType())){
+                        List<String> jVal = new ArrayList<>();
+                        for (int i=0;i<size; i++){
+                            jVal.add(PythonTypes.STR.toJava(val.get(i)));
+                        }
+                        data.putListString(strKey, jVal);
+                    }else if (Python.isinstance(item0, Python.intType())){
+                        List<Long> jVal = new ArrayList<>();
+                        for (int i=0;i<size; i++){
+                            jVal.add(PythonTypes.INT.toJava(val.get(i)));
+                        }
+                        data.putListInt64(strKey, jVal);
+                    }else if (Python.isinstance(item0, Python.floatType())){
+                        List<Double> jVal = new ArrayList<>();
+                        for (int i=0;i<size; i++){
+                            jVal.add(PythonTypes.FLOAT.toJava(val.get(i)));
+                        }
+                        data.putListDouble(strKey, jVal);
+                    }else if (Python.isinstance(item0, Python.boolType())){
+                        List<Boolean> jVal = new ArrayList<>();
+                        for (int i=0;i<size; i++){
+                            jVal.add(PythonTypes.BOOL.toJava(val.get(i)));
+                        }
+                        data.putListBoolean(strKey, jVal);
+                    } else if (Python.isinstance(item0, Python.memoryviewType())){
+                        List<byte[]> jVal = new ArrayList<>();
+                        for (int i=0;i<size; i++){
+                            jVal.add(PythonTypes.MEMORYVIEW.toJava(val.get(i)).getStringBytes());
+                        }
+                        data.putListBytes(strKey, jVal);
+                    }else if (Python.isinstance(item0, Python.importModule("numpy").attr("ndarray"))){
+                        List<NDArray> jVal = new ArrayList<>();
+                        for (int i=0;i<size; i++){
+                            jVal.add(NDArray.create(NumpyArray.INSTANCE.toJava(val.get(i))));
+                        }
+                        data.putListNDArray(strKey, jVal);
+                    }else if (Python.isinstance(item0, Python.importModule("PIL.Image").attr("Image"))){
+                        // TODO
+                        throw new PythonException("Image not supprted yet.");
+                    }else{ // Data
+                        List<Data> jVal = new ArrayList<>();
+                        for (int i=0;i<size; i++){
+                            jVal.add(toJava(val.get(i)));
+                        }
+                        data.putListData(strKey, jVal);
+                    }
+
+                }
+                else{ // Data
+                    Data jVal = toJava(val);
+                    data.put(strKey, jVal);
+                }
+            }
+            return data;
+        }
     }
 
     @Override
@@ -23,12 +128,15 @@ public class PyData extends PythonType<Data> {
 
     @Override
     public boolean accepts(Object javaObject) throws PythonException{
-        return false;
+        return javaObject instanceof Data;
     }
 
     @Override
     public Data adapt(Object javaObject) throws PythonException{
-        return null;
+        if (!(javaObject instanceof Data)){
+            throw new PythonException("Cannot cast " + javaObject.getClass() + " to Data.");
+        }
+        return (Data)javaObject;
     }
 
 
--- konduit-serving-python-new/src/main/resources/ai/konduit/serving/pydata.py
+++ konduit-serving-python-new/src/main/resources/ai/konduit/serving/pydata.py
@@ -1,10 +1,56 @@
+from PIL.Image import Image
+import numpy as np
+
+class BoundingBox(object):
+    def __init__(self, cx, cy, width, height, label='', probability=1.):
+        self.cx = cx
+        self.cy = cy
+        self.width = width
+        self.height = height
+        self.label = label
+        self.probability = probability
+
+    @classmethod
+    def fromXY(cls, x1, y1, x2, y2, label='', probability=1.):
+        cx = 0.5 * (x1 + x2)
+        cy = 0.5 * (y1 + y2)
+        w = x2 - x1
+        h = y2 - y1
+        return cls(cx, cy, w, h, label, probability)
+
+    @classmethod
+    def fromCWH(cls, cx, cy, width, height, label='', probability=1.):
+        return cls(cx, cy, width, height, label, probability)
+
+
 class Data(object):
+    @staticmethod
+    def _check_type(item):
+        atomic_types = (str, int, float, bool, bytes, memoryview, bytearray, Data, BoundingBox, np.ndarray, Image)
+        if isinstance(item, atomic_types):
+            pass
+        elif isinstance(item, (list, tuple)):
+            for subitem in item:
+                if not isinstance(subitem, atomic_types):
+                    if isinstance(subitem, (list, tuple)):
+                        raise Exception("Nested lists/tuples are not supported.")
+                    else:
+                        types_str = ', '.join([tp.__name__ for tp in atomic_types])
+                        raise Exception("Unsupported type: " + type(subitem).__name__ + ". Supported types are: " + types_str + ".")
+        else:
+            types_str = ', '.join([tp.__name__ for tp in atomic_types])
+            raise Exception("Unsupported type: " + type(item).__name__ + ". Supported types are: " + types_str + ".")
+
     def __init__(self):
         self.map = {}
         self.meta = None
 
     def __setitem__(self, key, value):
-        pass
+        assert isinstance(key, str)
+        Data._check_type(value)
+        if isinstance(value, (bytes, bytearray)):
+            value = memoryview(value)
+        self.map[key] = value
 
     def __getitem__(self, key):
         return self.map[key]
@@ -12,4 +58,11 @@ class Data(object):
     def __contains__(self, key):
         return key in self.map
 
+    def keys(self):
+        return self.map.keys()
 
+    def values(self):
+        return self.map.values()
+
+    def items(self):
+        return self.map.items()
