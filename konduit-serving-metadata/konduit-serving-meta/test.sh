b195112a50933fa13667baf79be14f608d7663bb
Alex Black <blacka101@gmail.com>
2020-06-05 14:01:37 +1000

    Test
    Signed-off-by: Alex Black <blacka101@gmail.com>

---
+++ konduit-serving-metadata/konduit-serving-meta/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo pwd
+cd target
+echo pwd
+mkdir -p extracted
+cd extracted
+jar xf ../konduit-serving-meta-0.1.0-SNAPSHOT.jar
+mv META-INF/konduit-serving/ai.konduit.serving.annotation.runner.CanRun META-INF/konduit-serving/PipelineStepRunner
+mv META-INF/konduit-serving/ai.konduit.serving.annotation.json.JsonName META-INF/konduit-serving/JsonNameMapping
+mv META-INF/konduit-serving/ai.konduit.serving.annotation.module.RequiresDependencies META-INF/konduit-serving/ModuleRequiresDependencies
+jar cf ../konduit-serving-meta-0.1.0-SNAPSHOT.jar META-INF/konduit-serving/PipelineStepRunner META-INF/konduit-serving/JsonNameMapping META-INF/konduit-serving/ModuleRequiresDependencies
