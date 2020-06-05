818f4c5fa13aac13970e6ea85c6e1f59a97fc07b
Alex Black <blacka101@gmail.com>
2020-06-05 14:15:12 +1000

    Test
    Signed-off-by: Alex Black <blacka101@gmail.com>

--- konduit-serving-metadata/konduit-serving-meta/test.sh
+++ konduit-serving-metadata/konduit-serving-meta/test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-echo pwd
+ls
 cd target
-echo pwd
+ls
 mkdir -p extracted
 cd extracted
+ls
 jar xf ../konduit-serving-meta-0.1.0-SNAPSHOT.jar
 mv META-INF/konduit-serving/ai.konduit.serving.annotation.runner.CanRun META-INF/konduit-serving/PipelineStepRunner
 mv META-INF/konduit-serving/ai.konduit.serving.annotation.json.JsonName META-INF/konduit-serving/JsonNameMapping
