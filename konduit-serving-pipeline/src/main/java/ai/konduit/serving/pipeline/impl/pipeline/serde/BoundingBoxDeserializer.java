9a6b8102a86de35ce1d0049505a6bfcc2dc25dd2
Alex Black <blacka101@gmail.com>
2020-06-19 22:38:01 +1000

    Add image crop pipeline step (#383)
    * Add ImageCropStep
    
    Signed-off-by: Alex Black <blacka101@gmail.com>
    
    * Fixes and test list
    
    Signed-off-by: Alex Black <blacka101@gmail.com>
    
    * Ignore for manual test
    
    Signed-off-by: Alex Black <blacka101@gmail.com>
    
    * JSON/YAML tests; fix BoundingBox JSON/YAML ser/de outside of Data
    
    Signed-off-by: Alex Black <blacka101@gmail.com>

---
+++ konduit-serving-pipeline/src/main/java/ai/konduit/serving/pipeline/impl/pipeline/serde/BoundingBoxDeserializer.java
@@ -0,0 +1,37 @@
+/*
+ *  ******************************************************************************
+ *  * Copyright (c) 2020 Konduit K.K.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Apache License, Version 2.0 which is available at
+ *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  * License for the specific language governing permissions and limitations
+ *  * under the License.
+ *  *
+ *  * SPDX-License-Identifier: Apache-2.0
+ *  *****************************************************************************
+ */
+
+package ai.konduit.serving.pipeline.impl.pipeline.serde;
+
+import ai.konduit.serving.pipeline.api.data.BoundingBox;
+import ai.konduit.serving.pipeline.impl.serde.DataJsonDeserializer;
+import org.nd4j.shade.jackson.core.JsonParser;
+import org.nd4j.shade.jackson.core.JsonProcessingException;
+import org.nd4j.shade.jackson.databind.DeserializationContext;
+import org.nd4j.shade.jackson.databind.JsonDeserializer;
+import org.nd4j.shade.jackson.databind.JsonNode;
+
+import java.io.IOException;
+
+public class BoundingBoxDeserializer extends JsonDeserializer<BoundingBox> {
+    @Override
+    public BoundingBox deserialize(JsonParser jp, DeserializationContext dc) throws IOException, JsonProcessingException {
+        JsonNode n = jp.getCodec().readTree(jp);
+        return DataJsonDeserializer.deserializeBB(n);
+    }
+}
