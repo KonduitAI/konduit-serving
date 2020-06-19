b6a73a48801d37ec60b72dc0e344e73dc8ade567
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2020-06-19 14:10:34 +0300

    nd4j -> Java SE
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- konduit-serving-pipeline/src/test/java/ai/konduit/serving/pipeline/impl/step/TestClassifierOutputStep.java
+++ konduit-serving-pipeline/src/test/java/ai/konduit/serving/pipeline/impl/step/TestClassifierOutputStep.java
@@ -6,7 +6,6 @@ import ai.konduit.serving.pipeline.api.pipeline.Pipeline;
 import ai.konduit.serving.pipeline.impl.pipeline.SequencePipeline;
 import ai.konduit.serving.pipeline.impl.step.ml.classifier.ClassifierOutputStep;
 import org.junit.Test;
-import org.nd4j.linalg.factory.Nd4j;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,39 +13,44 @@ import java.util.List;
 public class TestClassifierOutputStep {
 
 
-
     @Test
-    public void testcase1(){
-
-        List<String> labelsList = new ArrayList<String>();
-        labelsList.add("apple");
-        labelsList.add("banana");
-        labelsList.add("orange");
-
-        Pipeline p = SequencePipeline.builder()
-                .add(new ClassifierOutputStep()
-                        .inputName("preds")
-                        .returnIndex(true)
-                        .returnLabel(true)
-                        .returnProb(true)
-                        .allProbabilities(true)
-                        .labelName("label")
-                        .indexName("index")
-                        .probName("prob")
-                        .Labels(labelsList))
-                .build();
+    public void testcase1() {
 
 
         int bS = 1;
         int numClasses = 3;
 
-        NDArray preds = NDArray.create(Nd4j.rand(bS,numClasses));
-        Data in = Data.singleton("preds", preds);
-        Data out = p.executor().exec(in);
+        double values[][] = new double[bS][numClasses];
+        for (int i = 0; i < values.length; i++) {
+            for (int j = 0; j < values[i].length; j++) {
+                values[i][j] = (Math.random() * 10);
+            }
+
+            List<String> labelsList = new ArrayList<String>();
+            labelsList.add("apple");
+            labelsList.add("banana");
+            labelsList.add("orange");
+
+            Pipeline p = SequencePipeline.builder()
+                    .add(new ClassifierOutputStep()
+                            .inputName("preds")
+                            .returnIndex(true)
+                            .returnLabel(true)
+                            .returnProb(true)
+                            .allProbabilities(true)
+                            .labelName("label")
+                            .indexName("index")
+                            .probName("prob")
+                            .Labels(labelsList))
+                    .build();
 
 
+            NDArray preds = NDArray.create(values);
+            Data in = Data.singleton("preds", preds);
+            Data out = p.executor().exec(in);
+
+        }
 
 
     }
-
 }
