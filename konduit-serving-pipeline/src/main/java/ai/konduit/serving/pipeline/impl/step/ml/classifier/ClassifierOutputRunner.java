c4201c83f63cbed5570f947475c56e5a06418930
Andrii T <39699084+atuzhykov@users.noreply.github.com>
2020-06-23 17:35:19 +0300

    RegressionOutputStep (#385)
    * init
    
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>
    
    * test added
    
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>
    
    * small improvements
    
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>
    
    * iterating over batches fixed
    
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>
    
    * minor fixes for step and tests
    
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>
    
    * small improvements v2
    
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>
    
    * requested changes
    
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>
    
    * some refactoring (common methods to Utils and tests improvements)
    
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>
    
    * Small fixes
    
    Signed-off-by: Alex Black <blacka101@gmail.com>
    
    Co-authored-by: Alex Black <blacka101@gmail.com>

--- konduit-serving-pipeline/src/main/java/ai/konduit/serving/pipeline/impl/step/ml/classifier/ClassifierOutputRunner.java
+++ konduit-serving-pipeline/src/main/java/ai/konduit/serving/pipeline/impl/step/ml/classifier/ClassifierOutputRunner.java
@@ -22,7 +22,6 @@ import ai.konduit.serving.annotation.runner.CanRun;
 import ai.konduit.serving.pipeline.api.context.Context;
 import ai.konduit.serving.pipeline.api.data.Data;
 import ai.konduit.serving.pipeline.api.data.NDArray;
-import ai.konduit.serving.pipeline.api.data.NDArrayType;
 import ai.konduit.serving.pipeline.api.data.ValueType;
 import ai.konduit.serving.pipeline.api.step.PipelineStep;
 import ai.konduit.serving.pipeline.api.step.PipelineStepRunner;
@@ -34,8 +33,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
+
+import static ai.konduit.serving.pipeline.util.NDArrayUtils.*;
 
 @AllArgsConstructor
 @CanRun(ClassifierOutputStep.class)
@@ -177,48 +176,9 @@ public class ClassifierOutputRunner implements PipelineStepRunner {
     }
 
 
-    public static double[] squeeze(NDArray arr) {
 
-        // we have [numClasses] array, so do not modify nothing
-        if (arr.shape().length == 1) {
-            return arr.getAs(double[].class);
-        }
 
-        // i.e we have [1, numClasses] array
-        if (arr.shape().length == 2 && arr.shape()[0] == 1) {
-            return arr.getAs(double[][].class)[0];
-        }
 
-        throw new UnsupportedOperationException("Failed squeezing NDArray");
-
-    }
-
-    double[] getMaxValueAndIndex(double[] arr) {
-        double max = arr[0];
-        int maxIdx = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                maxIdx = i;
-            }
-        }
-        return new double[]{max, maxIdx};
-    }
-
-    NDArray FloatNDArrayToDouble(NDArray ndarr) {
-        if (ndarr.type() == NDArrayType.FLOAT || ndarr.type() == NDArrayType.FLOAT16 || ndarr.type() == NDArrayType.BFLOAT16) {
-            float[][] farr = ndarr.getAs(float[][].class);
-            double[][] darr = new double[(int) ndarr.shape()[0]][(int) ndarr.shape()[1]];
-            for (int i = 0; i < farr.length; i++) {
-                for (int j = 0; j < farr[i].length; j++) {
-                    darr[i][j] = Double.valueOf(farr[i][j]);
-                }
-            }
-
-            return NDArray.create(darr);
-        }
-        return ndarr;
-    }
 
 
 }
