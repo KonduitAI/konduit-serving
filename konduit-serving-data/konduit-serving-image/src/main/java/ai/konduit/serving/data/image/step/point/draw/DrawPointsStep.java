965c980d74910e19d6e0224dcb3f8f77b5ac4815
Paul Dubs <paul.dubs@gmail.com>
2020-06-24 11:52:17 +0200

    Implement DrawPointsStep
    Signed-off-by: Paul Dubs <paul.dubs@gmail.com>

---
+++ konduit-serving-data/konduit-serving-image/src/main/java/ai/konduit/serving/data/image/step/point/draw/DrawPointsStep.java
@@ -0,0 +1,86 @@
+/*
+ *  ******************************************************************************
+ *  * Copyright (c) 2020 Konduit K.K.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Apache License, Version 2.0 which is available at
+ *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  * License for the specific language governing permissions and limitations
+ *  * under the License.
+ *  *
+ *  * SPDX-License-Identifier: Apache-2.0
+ *  *****************************************************************************
+ */
+
+package ai.konduit.serving.data.image.step.point.draw;
+
+import ai.konduit.serving.annotation.json.JsonName;
+import ai.konduit.serving.pipeline.api.step.PipelineStep;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Singular;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Draw 2D points<br>
+ * Configuration:<br>
+ * <ul>
+ *     <li><b>classColors</b>: Optional: A list of colors to use for each class. Must be in one of the following formats: hex/HTML - "#788E87",
+ *             RGB - "rgb(128,0,255)", or one of 16 HTML color name such as \"green\" (https://en.wikipedia.org/wiki/Web_colors#HTML_color_names).
+ *             If no colors are specified, or not enough colors are specified, random colors are used instead (note consistent between runs).
+ *             Colors are mapped to named classes in alphabetical order, i.e. first color to class A, second color to class B, etc...</li>
+ *     <li><b>points</b>: Names of the points to be drawn. Accepts both single points and lists of points.
+ *     <li><b>image</b>: Optional. Name of the image to use as size reference</li>
+ *     <li><b>width</b>: Must be provided when <b>image</b> isn't set. Used to resolve position of points with relative addressing (dimensions between 0 and 1)</li>
+ *     <li><b>height</b>: Must be provided when <b>image</b> isn't set. Used to resolve position of points with relative addressing (dimensions between 0 and 1)</li>
+ *     <li><b>outputName</b>: Name of the output image</li>
+ * </ul>
+ *
+ * @author Paul Dubs
+ */
+@Builder
+@Data
+@Accessors(fluent = true)
+@AllArgsConstructor
+@JsonName("DRAW_POINTS")
+@Schema(description = "A pipeline step that configures how to draw 2D points, optionally on an image.")
+public class DrawPointsStep implements PipelineStep {
+    public static final String DEFAULT_OUTPUT_NAME = "image";
+
+    @Schema(description = "This is an optional field which specifies the mapping of colors to use for each class. " +
+            "The color can be a hex/HTML string like" +
+            "\"#788E87\", an RGB value like RGB - \"rgb(128,0,255)\" or  it can be from a set of predefined HTML color names: " +
+            "[white, silver, gray, black, red, maroon, yellow, olive, lime, green, aqua, teal, blue, navy, fuchsia, purple]")
+    private Map<String, String> classColors;
+
+    @Schema(description = "Names of the points to be drawn. Accepts both single points and lists of points.")
+    @Singular
+    private List<String> points;
+
+    @Schema(description = "Point radius on drawn image. ")
+    private Integer radius;
+
+    @Schema(description = "An optional field, specifying the name of the image to use as size reference")
+    private String image;
+
+    @Schema(description = "Must be provided when \"image\" isn't set. Used to resolve position of points with relative addressing (dimensions between 0 and 1)")
+    private Integer width;
+
+    @Schema(description = "Must be provided when \"image\" isn't set. Used to resolve position of points with relative addressing (dimensions between 0 and 1)")
+    private Integer height;
+
+
+
+    @Schema(description = "Name of the output image",
+            defaultValue = DEFAULT_OUTPUT_NAME)
+    private String outputName;
+}
---
+++ konduit-serving-data/konduit-serving-image/src/test/java/ai/konduit/serving/data/image/TestDrawPointsStep.java
@@ -0,0 +1,67 @@
+/*
+ *  ******************************************************************************
+ *  * Copyright (c) 2020 Konduit K.K.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Apache License, Version 2.0 which is available at
+ *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  * License for the specific language governing permissions and limitations
+ *  * under the License.
+ *  *
+ *  * SPDX-License-Identifier: Apache-2.0
+ *  *****************************************************************************
+ */
+
+package ai.konduit.serving.data.image;
+
+import ai.konduit.serving.data.image.step.point.draw.DrawPointsStep;
+import ai.konduit.serving.data.image.step.show.ShowImagePipelineStep;
+import ai.konduit.serving.pipeline.api.data.Data;
+import ai.konduit.serving.pipeline.api.data.Point;
+import ai.konduit.serving.pipeline.api.pipeline.Pipeline;
+import ai.konduit.serving.pipeline.impl.pipeline.SequencePipeline;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TestDrawPointsStep {
+
+    @Ignore
+    @Test
+    public void testNoReferenceImage() throws Exception {
+        HashMap<String, String> colorMap = new HashMap<>();
+        colorMap.put("foo", "red");
+        colorMap.put("bar", "green");
+
+        Pipeline p = SequencePipeline.builder()
+                .add(DrawPointsStep.builder()
+                        .classColors(colorMap)
+                        .point("points")
+                        .width(100)
+                        .height(200)
+                        .outputName("image")
+                        .build())
+                .add(new ShowImagePipelineStep())
+                .build();
+
+        Data data = Data.empty();
+        data.putListPoint("points",
+                Arrays.asList(
+                        Point.create(2, 2, "bar", 0.1),
+                        Point.create(0.1, 0.1, "foo", 0.2),
+                        Point.create(0.5, 0.8, "bar", 0.3),
+                        Point.create(30, 59, "foo", 0.4)
+                )
+        );
+
+        Data out = p.executor().exec(data);
+
+        Thread.sleep(Long.MAX_VALUE);
+    }
+}
