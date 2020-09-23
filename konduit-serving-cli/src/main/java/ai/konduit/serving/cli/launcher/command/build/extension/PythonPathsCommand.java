f80cf7586a394fff4e19cabbd3d504496504e6f8
shams <shamsazeem20@gmail.com>
2020-09-23 17:20:29 +0500

    Listing python installs
    Signed-off-by: shams <shamsazeem20@gmail.com>

--- konduit-serving-cli/src/main/java/ai/konduit/serving/cli/launcher/command/build/extension/PythonPathsCommand.java
+++ konduit-serving-cli/src/main/java/ai/konduit/serving/cli/launcher/command/build/extension/PythonPathsCommand.java
@@ -21,8 +21,17 @@ package ai.konduit.serving.cli.launcher.command.build.extension;
 import io.vertx.core.cli.annotations.*;
 import io.vertx.core.spi.launcher.DefaultCommand;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.nd4j.common.base.Preconditions;
+import oshi.PlatformEnum;
+import oshi.SystemInfo;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Name("pythonpaths")
 @Summary("A utility command to manage system installed and manually registered python binaries.")
@@ -44,6 +53,8 @@ import java.util.Arrays;
 @Slf4j
 public class PythonPathsCommand extends DefaultCommand {
 
+    public static PlatformEnum currentPlatformEnum = SystemInfo.getCurrentPlatformEnum();
+    public static String FINDER_COMMAND = currentPlatformEnum == PlatformEnum.WINDOWS || currentPlatformEnum == PlatformEnum.MACOSX ? "where" : "which";
     private SubCommand subCommand;
     private Object type;
     private String path;
@@ -63,38 +74,14 @@ public class PythonPathsCommand extends DefaultCommand {
         }
     }
 
-    @Option(shortName = "t", longName = "type", argName = "python_type")
-    @DefaultValue("PYTHON")
+    @Option(shortName = "t", longName = "type", argName = "type", required = true)
     @Description("Name of the python type. For the 'add' subcommand, accepted values are: [python, conda, venv]. " +
             "For the 'list' subcommand, accepted values are: [all, javacpp, python, conda, venv]")
     public void setType(String type) {
-        switch (this.subCommand) {
-            case ADD:
-                try {
-                    this.type = PythonPathsCommand.Type.valueOf(type.toUpperCase());
-                } catch (Exception e) {
-                    System.out.format("Invalid type name: '%s'. Allowed values are: %s -> (case insensitive).",
-                            subCommand, Arrays.toString(PythonPathsCommand.Type.values()));
-                    System.exit(1);
-                }
-                break;
-            case LIST:
-                try {
-                    this.type = PythonPathsCommand.ListInstallationType.valueOf(type.toUpperCase());
-                } catch (Exception e) {
-                    System.out.format("Invalid type name: '%s'. Allowed values are: %s -> (case insensitive).",
-                            subCommand, Arrays.toString(PythonPathsCommand.ListInstallationType.values()));
-                    System.exit(1);
-                }
-                break;
-            default:
-                System.out.format("Invalid sub command name: '%s'. Allowed values are: %s -> (case insensitive).",
-                        subCommand, Arrays.toString(PythonPathsCommand.SubCommand.values()));
-        }
+        this.type = type;
     }
 
     @Option(shortName = "p", longName = "path", argName = "install_path")
-    @DefaultValue("PYTHON")
     @Description("Absolute path of the python installation. For conda and venv types this refers to the absolute path " +
             "of the root installation folder.")
     public void setPath(String path) {
@@ -102,7 +89,6 @@ public class PythonPathsCommand extends DefaultCommand {
     }
 
     @Option(shortName = "wip", longName = "with-installed-packages", flag = true)
-    @DefaultValue("PYTHON")
     @Description("Absolute path of the python installation. For conda and venv types this refers to the absolute path " +
             "of the root installation folder.")
     public void setPath(boolean withInstalledPackages) {
@@ -123,6 +109,30 @@ public class PythonPathsCommand extends DefaultCommand {
 
     @Override
     public void run() {
+        switch (this.subCommand) {
+            case ADD:
+                try {
+                    this.type = PythonPathsCommand.Type.valueOf(((String) type).toUpperCase());
+                } catch (Exception e) {
+                    out.format("Invalid type name: '%s'. Allowed values are: %s -> (case insensitive).",
+                            subCommand, Arrays.toString(PythonPathsCommand.Type.values()));
+                    System.exit(1);
+                }
+                break;
+            case LIST:
+                try {
+                    this.type = PythonPathsCommand.ListInstallationType.valueOf(((String) type).toUpperCase());
+                } catch (Exception e) {
+                    out.format("Invalid type name: '%s'. Allowed values are: %s -> (case insensitive).",
+                            subCommand, Arrays.toString(PythonPathsCommand.ListInstallationType.values()));
+                    System.exit(1);
+                }
+                break;
+            default:
+                out.format("Invalid sub command name: '%s'. Allowed values are: %s -> (case insensitive).",
+                        subCommand, Arrays.toString(PythonPathsCommand.SubCommand.values()));
+        }
+
         switch (subCommand) {
             case ADD:
                 registerInstallation();
@@ -163,23 +173,79 @@ public class PythonPathsCommand extends DefaultCommand {
         }
     }
 
-    private static void listVenvInstallations() {
+    private static void listJavacppInstallations() {
 
+    }
+
+    private static void listPythonInstallations() {
+        List<String> paths = findInstallationPaths("python");
+
+        System.out.println("\n----------------------------PYTHON INSTALLS----------------------------");
+        System.out.print(
+                IntStream.range(0, paths.size())
+                        .mapToObj(index -> formatPythonInstallation(paths.get(index), String.valueOf(index + 1)))
+                        .collect(Collectors.joining(System.lineSeparator()))
+        );
+        System.out.println("-----------------------------------------------------------------------");
+    }
+
+    private static String formatPythonInstallation(String pythonPath, String pythonId) {
+        return String.format(" -\tid: %s%n\t%s%n\t%s", pythonId, pythonPath, runAndGetOutput(pythonPath, "-V"));
     }
 
     private static void listCondaInstallations() {
 
     }
 
-    private static void listPythonInstallations() {
-
-    }
-
-    private static void listJavacppInstallations() {
+    private static void listVenvInstallations() {
 
     }
 
     public static void registerInstallation() {
 
+    }
+
+    private static List<String> findInstallationPaths(String type) {
+        return Arrays.stream(runAndGetOutput(FINDER_COMMAND, type).split(System.lineSeparator())).collect(Collectors.toList());
+    }
+
+    private static String runAndGetOutput(String... command){
+        try {
+            Process process = startProcessFromCommand(command);
+
+            String output = getProcessOutput(process);
+            String errorOutput = getProcessErrorOutput(process);
+
+            Preconditions.checkState(0 == process.waitFor(),
+                    "Process exited with non-zero exit code. Details: \n" + output + "\n" + errorOutput
+            );
+
+            log.debug("Process output: {}", output);
+            log.debug("Process errors (ignore if none): '{}'", errorOutput);
+
+            return output;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            System.exit(1);
+            return null;
+        }
+    }
+
+    private static Process startProcessFromCommand(String... command) throws IOException {
+        return getProcessBuilderFromCommand(command).start();
+    }
+
+    private static ProcessBuilder getProcessBuilderFromCommand(String... command) {
+        List<String> fullCommand = Arrays.stream(command).collect(Collectors.toList());
+        log.debug("Running command (sub): {}", String.join(" ", command));
+        return new ProcessBuilder(fullCommand);
+    }
+
+    private static String getProcessOutput(Process process) throws IOException {
+        return IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
+    }
+
+    private static String getProcessErrorOutput(Process process) throws IOException {
+        return IOUtils.toString(process.getErrorStream(), StandardCharsets.UTF_8);
     }
 }
