f75640f0b2c700aff3a6102001614767c2a77250
shamsulazeem <shamsazeem20@gmail.com>
2020-12-01 18:32:00 +0500

    Implemented mqtt server along with SSL feature
    Signed-off-by: shamsulazeem <shamsazeem20@gmail.com>

--- konduit-serving-vertx-protocols/konduit-serving-mqtt/src/main/java/ai/konduit/serving/vertx/protocols/mqtt/verticle/InferenceVerticleMqtt.java
+++ konduit-serving-vertx-protocols/konduit-serving-mqtt/src/main/java/ai/konduit/serving/vertx/protocols/mqtt/verticle/InferenceVerticleMqtt.java
@@ -18,13 +18,194 @@
 
 package ai.konduit.serving.vertx.protocols.mqtt.verticle;
 
+import ai.konduit.serving.pipeline.api.data.Data;
+import ai.konduit.serving.pipeline.settings.constants.EnvironmentConstants;
 import ai.konduit.serving.vertx.verticle.InferenceVerticle;
+import com.google.common.base.Strings;
+import io.netty.handler.codec.mqtt.MqttQoS;
 import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.impl.ContextInternal;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.PemKeyCertOptions;
+import io.vertx.core.net.SelfSignedCertificate;
+import io.vertx.mqtt.MqttServer;
+import io.vertx.mqtt.MqttServerOptions;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+@Slf4j
 public class InferenceVerticleMqtt extends InferenceVerticle {
 
     @Override
     public void start(Promise<Void> startPromise) {
-        // todo: implement the mqtt server start here.
+        vertx.executeBlocking(handler -> {
+            try {
+                initialize();
+                handler.complete();
+            } catch (Exception exception) {
+                handler.fail(exception);
+                startPromise.fail(exception);
+            }
+
+        }, resultHandler -> {
+            if (resultHandler.failed()) {
+                if (resultHandler.cause() != null)
+                    startPromise.fail(resultHandler.cause());
+                else {
+                    startPromise.fail("Failed to start. Unknown cause.");
+                }
+            } else {
+                int port;
+
+                String portEnvValue = System.getenv(EnvironmentConstants.KONDUIT_SERVING_PORT);
+                if (portEnvValue != null) {
+                    try {
+                        port = Integer.parseInt(portEnvValue);
+                    } catch (NumberFormatException exception) {
+                        log.error("Environment variable \"{}={}\" isn't a valid port number.",
+                                EnvironmentConstants.KONDUIT_SERVING_PORT, portEnvValue);
+                        startPromise.fail(exception);
+                        return;
+                    }
+                } else {
+                    port = inferenceConfiguration.port();
+                }
+
+                if (port < 0 || port > 0xFFFF) {
+                    startPromise.fail(new Exception("Valid port range is 0 <= port <= 65535. The given port was " + port));
+                    return;
+                }
+
+                MqttServerOptions mqttServerOptions = new MqttServerOptions()
+                        .setHost(inferenceConfiguration.host())
+                        .setPort(inferenceConfiguration.port());
+
+                boolean useSsl = inferenceConfiguration.useSsl();
+                String sslKeyPath = inferenceConfiguration.sslKeyPath();
+                String sslCertificatePath = inferenceConfiguration.sslCertificatePath();
+
+                if (useSsl) {
+                    if (Strings.isNullOrEmpty(sslKeyPath) || Strings.isNullOrEmpty(sslCertificatePath)) {
+                        if (Strings.isNullOrEmpty(sslKeyPath)) {
+                            log.warn("No pem key file specified for SSL.");
+                        }
+
+                        if (Strings.isNullOrEmpty(sslCertificatePath)) {
+                            log.warn("No pem certificate file specified for SSL.");
+                        }
+
+                        log.info("Using an auto generated self signed pem key and certificate with SSL.");
+                        mqttServerOptions.setKeyCertOptions(SelfSignedCertificate.create().keyCertOptions());
+                    } else {
+                        sslKeyPath = new File(sslKeyPath).getAbsolutePath();
+                        sslCertificatePath = new File(sslCertificatePath).getAbsolutePath();
+                        log.info("Using SSL with PEM Key: {} and certificate {}.", sslKeyPath, sslCertificatePath);
+
+                        mqttServerOptions.setPemKeyCertOptions(new PemKeyCertOptions().setKeyPath(sslKeyPath).setCertPath(sslCertificatePath));
+                    }
+                }
+
+                MqttServer mqttServer = MqttServer.create(vertx, mqttServerOptions);
+                mqttServer.endpointHandler(
+                        endpoint -> {
+                            log.info("MQTT client [{}] request to connect, clean session = {}",
+                                    endpoint.clientIdentifier() ,
+                                    endpoint.isCleanSession());
+
+                            if (endpoint.auth() != null) {
+                                log.info("[username = {}, password = {}]",
+                                        endpoint.auth().getUsername(),
+                                        endpoint.auth().getPassword());
+                            }
+                            if (endpoint.will() != null) {
+                                log.info("[will topic = {} msg = {} QoS = {} isRetain = {}]",
+                                        endpoint.will().getWillTopic(),
+                                        endpoint.will().getWillMessageBytes(),
+                                        endpoint.will().getWillQos(),
+                                        endpoint.will().isWillRetain());
+                            }
+
+                            log.info("[keep alive timeout = {}]",
+                                    endpoint.keepAliveTimeSeconds());
+
+                            endpoint.accept(false)
+
+                                    .disconnectHandler(v -> log.info("Received disconnect from client"))
+
+                                    .subscribeHandler(handler ->
+                                            endpoint.subscribeAcknowledge(handler.messageId(),
+                                                    handler.topicSubscriptions().stream().map(subscription -> {
+                                                        log.info("Subscription for {} with QoS {}",
+                                                                subscription.topicName(),
+                                                                subscription.qualityOfService());
+
+                                                        return subscription.qualityOfService();
+                                                    }).collect(Collectors.toList())))
+                                    .unsubscribeHandler(handler -> {
+                                        handler.topics().forEach(topic -> log.info("Unsubscription for {}", topic));
+                                        endpoint.unsubscribeAcknowledge(handler.messageId());
+                                    })
+
+                                    .publishHandler(message -> {
+                                        String messageString = message.payload().toString(StandardCharsets.UTF_8);
+                                        log.info("Just received message [{}] with QoS [{}}]",
+                                                messageString,
+                                                message.qosLevel());
+
+                                        if (message.qosLevel() == MqttQoS.AT_LEAST_ONCE) {
+                                            endpoint.publishAcknowledge(message.messageId());
+                                        } else if (message.qosLevel() == MqttQoS.EXACTLY_ONCE) {
+                                            endpoint.publishReceived(message.messageId());
+                                        }
+
+                                        endpoint.publish(message.topicName(),
+                                                Buffer.buffer(pipelineExecutor.exec(Data.fromJson(messageString)).toJson()),
+                                                MqttQoS.EXACTLY_ONCE,
+                                                false,
+                                                false);
+                                    })
+
+                                    .publishReleaseHandler(endpoint::publishComplete)
+
+                                    .publishAcknowledgeHandler(messageId -> log.info("Received ack for message = {}", messageId))
+
+                                    .publishReceivedHandler(endpoint::publishRelease)
+
+                                    .publishCompletionHandler(messageId -> log.info("Received ack for message = {}", messageId))
+
+                            .pingHandler(v -> log.info("Ping received from client"));
+                        })
+                        .listen(handler -> {
+                            if (handler.failed()) {
+                                startPromise.fail(handler.cause());
+                            } else {
+                                int actualPort = handler.result().actualPort();
+
+                                inferenceConfiguration.port(actualPort);
+
+                                try {
+                                    ((ContextInternal) context).getDeployment()
+                                            .deploymentOptions()
+                                            .setConfig(new JsonObject(inferenceConfiguration.toJson()));
+
+                                    long pid = getPid();
+
+                                    saveInspectionDataIfRequired(pid);
+
+                                    log.info("HTTP server for kafka is listening on host: '{}'", inferenceConfiguration.host());
+                                    log.info("HTTP server for kafka started on port {}", actualPort);
+
+                                    startPromise.complete();
+                                } catch (Throwable throwable) {
+                                    startPromise.fail(throwable);
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
