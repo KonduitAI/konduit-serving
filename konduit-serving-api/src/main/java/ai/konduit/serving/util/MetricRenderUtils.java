2f1c67daceafa74f8c4001e6968a328ca598adf5
Adam Gibson <adam@konduit.ai>
2022-08-18 19:13:33 +0900

    Update Copyright, dl4j version for release

--- konduit-serving-api/src/main/java/ai/konduit/serving/util/MetricRenderUtils.java
+++ konduit-serving-api/src/main/java/ai/konduit/serving/util/MetricRenderUtils.java
@@ -2,7 +2,7 @@
  *
  *  * ******************************************************************************
  *  *
- *  *  * Copyright (c) 2019 Konduit AI.
+ *  *  * Copyright (c) 2022 Konduit K.K.
  *  *  *
  *  *  * This program and the accompanying materials are made available under the
  *  *  * terms of the Apache License, Version 2.0 which is available at
